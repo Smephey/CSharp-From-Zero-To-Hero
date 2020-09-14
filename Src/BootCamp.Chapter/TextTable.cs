5df15dea9ebd4bff9a06b810c16ba0d66f3e7819
Smephey <smephey@gmail.com>
2020-09-14 13:18:57 +0100

    Clean up TextTable. This part of the homework is ready for submission so getting rid of comments

--- Src/BootCamp.Chapter/TextTable.cs
+++ Src/BootCamp.Chapter/TextTable.cs
@@ -5,50 +5,17 @@ using System.Text;
 
 namespace BootCamp.Chapter
 {
-    /// <summary>
-    /// Part 1.
-    /// </summary>
     public static class TextTable
     {
-        /*
-
-         Input: "Hello", 0
-           +-----+
-           |Hello|
-           +-----+
-           
-         Input: $"Hello{Environment.NewLine}World!", 0
-           +------+
-           |/Hello |
-           |World!|
-           +------+
-           
-         Input: "Hello", 1
-           +-------+
-           |       |
-           | Hello |
-           |       |
-           +-------+
-
-         */
-
-        /// <summary>
-        /// Build a table for given message with given padding.
-        /// Padding means how many spaces will a message be wrapped with.
-        /// Table itself is made of: "+-" symbols. 
-        /// </summary>
         public static string Build(string message, int padding)
         {
             if (string.IsNullOrEmpty(message)) return "";
 
             StringBuilder sb = new StringBuilder();
-
-            //determine message size
             var allWords = SplitWords(message);
             var longestWordLength = FindLongestWordLength(allWords);
             var horizontalLineWidth = longestWordLength + 2 * padding;
 
-            //build box
             BuildHorizontalLine(horizontalLineWidth, sb);
             BuildPadding(padding, horizontalLineWidth, sb);
             for (var i = 0; i < allWords.Length; i++)
@@ -59,7 +26,6 @@ namespace BootCamp.Chapter
                 sb.Append("|");
             }
             BuildPadding(padding, horizontalLineWidth, sb);
-
             sb.Append(Environment.NewLine);
             BuildHorizontalLine(horizontalLineWidth, sb);
             sb.Append(Environment.NewLine);
